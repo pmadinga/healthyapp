0a47c66ca7b5fca9b594dbf269fa121558682e67
Phiwa Madinga <phiwa@koedr.org>
2022-08-23 19:14:45 +0200

    Added site header

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import Logo from '../img/Logo.png'
+import GitHubLogo from '../img/GitHubLogo.png'
+import styles from '../styles/SiteHeader.module.css'
+import Link from "next/link";
 
 const SiteHeader = () => {
   return(
-    <nav>
-      <div>
-        <Image src={Logo} width={50} height={50} alt="Logo"/>
-      </div>
+    <nav className={styles.nav}>
+        <div className={styles.inline_logo}>
+          <Image src={Logo} width={35} height={35} alt="Logo"/>
+          <h1>Healthy</h1>
+        </div>
+        <Link href={'https://github.com/pmadinga/healthyapp'} className={styles.git}>
+          <a>
+            <Image className={styles.git} src={GitHubLogo} width={35} height={35} alt='GitHub Logo'/>
+          </a>
+        </Link>
     </nav>
   )
 }
